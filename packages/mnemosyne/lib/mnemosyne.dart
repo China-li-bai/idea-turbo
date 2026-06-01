38ae931b2c58153023a94478677a76fa0dc75fc7
li <1272679088@qq.com>
2026-06-01 21:28:07 +0800

    refactor: 移除未使用的npc和pet模块导出
    1. 从social.dart和主导出文件中删除了未使用的npc和pet模块导出
    2. 新增了宠物模块相关的单元测试文件，覆盖内存桥和编排器功能

--- packages/mnemosyne/lib/mnemosyne.dart
+++ packages/mnemosyne/lib/mnemosyne.dart
@@ -3,7 +3,6 @@ library mnemosyne;
 export 'package:mnemosyne/core/core.dart';
 export 'package:mnemosyne/features/memory/memory.dart';
 export 'package:mnemosyne/features/xiang/xiang.dart';
-export 'package:mnemosyne/features/pet/pet.dart';
 export 'package:mnemosyne/services/services.dart';
 export 'package:mnemosyne/utils/utils.dart';
 export 'package:mnemosyne/mnemosyne_class.dart';
